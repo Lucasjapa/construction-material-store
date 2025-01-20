475bce56caece40ba1891e8fc667d42262481d57
Lucas Silva <lucascdsilva.bj@gmail.com>
2025-01-20 18:59:08 -0300

    Finalização da componentização da parte de produto

--- src/main/java/projectpoo/construction_material_store/screens/ProductFrame.java
+++ src/main/java/projectpoo/construction_material_store/screens/ProductFrame.java
@@ -1,10 +1,6 @@
 package projectpoo.construction_material_store.screens;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-import projectpoo.construction_material_store.domain.Product;
 import projectpoo.construction_material_store.dto.ProductDTO;
 import projectpoo.construction_material_store.screens.components.ProductModal;
 import projectpoo.construction_material_store.screens.components.TableComponent;
@@ -12,7 +8,6 @@ import projectpoo.construction_material_store.screens.components.TableComponent;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,26 +35,7 @@ public class ProductFrame extends JFrame {
 
         // Cria a instância do TableComponent
         TableComponent tableComponent = new TableComponent(productPanel);
-        ProductModal productModal = new ProductModal();
-
-        // Painel de botões
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
-
-        // Adiciona um botão para criar um novo produto
-        JButton btnCreateProduct = new JButton("Criar Produto");
-        btnCreateProduct.addActionListener(e -> productModal.openCreateProductDialog(tableComponent));
-        buttonPanel.add(btnCreateProduct);
-
-        // Adiciona um botão para criar um novo produto
-        JButton btnUpdateProduct = new JButton("Editar Produto");
-        btnUpdateProduct.addActionListener(e -> updateSelectedProduct(tableComponent));
-        buttonPanel.add(btnUpdateProduct);
-
-        // Botão para deletar os produtos selecionados
-        JButton btnDeleteSelected = new JButton("Deletar Selecionados");
-        btnDeleteSelected.addActionListener(e -> deleteSelectedProducts(tableComponent));
-        buttonPanel.add(btnDeleteSelected);
+        JPanel buttonPanel = getButtonPanel(tableComponent);
 
         // Adiciona o painel de botões ao layout no local apropriado (parte inferior)
         add(buttonPanel, BorderLayout.SOUTH);
@@ -72,123 +48,31 @@ public class ProductFrame extends JFrame {
         }
     }
 
-    private void updateCreateProductDialog(TableComponent tableComponent, ProductDTO productDto) {
-        // Criar uma instância do diálogo
-        JDialog dialog = new JDialog(this, "Editar Produto", true);
-        dialog.setSize(650, 400);
-        dialog.setLocationRelativeTo(this);
+    private JPanel getButtonPanel(TableComponent tableComponent) {
+        ProductModal productModal = new ProductModal();
 
-        // Painel para os campos
-        JPanel panel = new JPanel(new GridLayout(13, 2, 5, 5));
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        // Painel de botões
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
 
-        // Campos de entrada
-        panel.add(new JLabel("Nome:"));
-        JTextField nameField = new JTextField(productDto.getName());
-        panel.add(nameField);
+        // Adiciona um botão para criar um novo produto
+        JButton btnCreateProduct = new JButton("Criar Produto");
+        btnCreateProduct.addActionListener(e -> productModal.productActionModal(tableComponent, null));
+        buttonPanel.add(btnCreateProduct);
 
-        panel.add(new JLabel("Código do Produto:"));
-        JTextField codProductField = new JTextField(productDto.getCodProduct());
-        panel.add(codProductField);
+        // Adiciona um botão para criar um novo produto
+        JButton btnUpdateProduct = new JButton("Editar Produto");
+        btnUpdateProduct.addActionListener(e -> updateSelectedProduct(tableComponent, productModal));
+        buttonPanel.add(btnUpdateProduct);
 
-        panel.add(new JLabel("Descrição:"));
-        JTextField descriptionField = new JTextField(productDto.getCodProduct());
-        panel.add(descriptionField);
-
-        panel.add(new JLabel("Quantidade em Estoque:"));
-        JTextField totalStockField = new JTextField(String.valueOf(productDto.getTotalStock()));
-        panel.add(totalStockField);
-
-        panel.add(new JLabel("Estoque Mínimo:"));
-        JTextField minStockField = new JTextField(String.valueOf(productDto.getMinStock()));
-        panel.add(minStockField);
-
-        panel.add(new JLabel("Preço:"));
-        JTextField priceField = new JTextField(String.valueOf(productDto.getPrice()));
-        panel.add(priceField);
-
-        panel.add(new JLabel("Unidade de Venda:"));
-        JTextField salesUnitField = new JTextField(productDto.getSalesUnit());
-        panel.add(salesUnitField);
-
-        panel.add(new JLabel("Data de Validade: (dd-mm-aaaa)"));
-        JTextField expirationDateField = new JTextField(productDto.getExpirationDate());
-        panel.add(expirationDateField);
-
-        panel.add(new JLabel("Categorias:"));
-        JComboBox<String> categoryList = new JComboBox<>(new String[]{
-                "Cimento e Argamassas", "Tintas e Acessórios", "Ferramentas", "Materiais Elétricos",
-                "Materiais Hidráulicos", "Madeiras e Compensados", "Ferragens", "Pisos e Revestimentos",
-                "Iluminação", "Decoração", "Jardinagem", "Outros"
-        });
-        // Define a categoria selecionada com base no valor do DTO
-        categoryList.setSelectedItem(productDto.getCategory());
-        panel.add(categoryList);
-
-        // Botão de salvar
-        JButton btnSave = new JButton("Salvar");
-        btnSave.addActionListener(e -> {
-            productDto.setName(nameField.getText());
-            productDto.setCodProduct(codProductField.getText());
-            productDto.setDescription(descriptionField.getText());
-            productDto.setTotalStock(Double.parseDouble(totalStockField.getText()));
-            productDto.setMinStock(Double.parseDouble(minStockField.getText()));
-            productDto.setPrice(Double.parseDouble(priceField.getText()));
-            productDto.setSalesUnit(salesUnitField.getText());
-            productDto.setExpirationDate(expirationDateField.getText());
-            productDto.setCategory(categoryList.getSelectedItem().toString());
-
-            // Validação dos campos
-            if (productDto.getName().isEmpty() || productDto.getCodProduct().isEmpty() || productDto.getSalesUnit().isEmpty()) {
-                JOptionPane.showMessageDialog(dialog, "Todos os campos são obrigatórios.", "Erro", JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-
-            try {
-                if (productDto.getTotalStock() < 0 || productDto.getMinStock() < 0 || productDto.getPrice() < 0) {
-                    JOptionPane.showMessageDialog(dialog, "Valores numéricos não podem ser negativos.", "Erro", JOptionPane.ERROR_MESSAGE);
-                    return;
-                }
-
-                // Criar produto
-                btnSave.setEnabled(false); // Desabilita o botão para evitar múltiplos cliques
-                updateProduct(productDto);
-                JOptionPane.showMessageDialog(dialog, "Produto atualizado com sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
-                tableComponent.loadData(API_URL, ProductDTO[].class); // Atualizar lista de produtos
-                dialog.dispose(); // Fechar o diálogo
-            } catch (DateTimeParseException ex) {
-                JOptionPane.showMessageDialog(dialog, "Formato de data inválido. Use dd-mm-aaaa.", "Erro", JOptionPane.ERROR_MESSAGE);
-            } catch (NumberFormatException ex) {
-                JOptionPane.showMessageDialog(dialog, "Insira valores numéricos válidos para estoque e preço.", "Erro", JOptionPane.ERROR_MESSAGE);
-            } catch (Exception ex) {
-                JOptionPane.showMessageDialog(dialog, "Erro ao atualizar produto", "Erro", JOptionPane.ERROR_MESSAGE);
-                ex.printStackTrace();
-            } finally {
-                btnSave.setEnabled(true); // Reativa o botão, mesmo em caso de erro
-            }
-        });
-
-        panel.add(new JLabel()); // Espaço vazio para alinhar o botão
-        panel.add(btnSave);
-
-        dialog.add(panel);
-        dialog.setVisible(true);
+        // Botão para deletar os produtos selecionados
+        JButton btnDeleteSelected = new JButton("Deletar Selecionados");
+        btnDeleteSelected.addActionListener(e -> deleteSelectedProducts(tableComponent));
+        buttonPanel.add(btnDeleteSelected);
+        return buttonPanel;
     }
 
-    private void updateProduct(ProductDTO productDto) throws Exception {
-        RestTemplate restTemplate = new RestTemplate();
-
-        // Envia a requisição PUT com o identificador do produto
-        String url = API_URL + "/" + productDto.getId();  // Supondo que o ID do produto seja parte da URL para atualização
-        HttpEntity<ProductDTO> request = new HttpEntity<>(productDto);
-        ResponseEntity<Product> response = restTemplate.exchange(url, HttpMethod.PUT, request, Product.class);
-
-        if (!response.getStatusCode().is2xxSuccessful()) {
-            throw new Exception("Erro ao atualizar produto: " + response.getStatusCode());
-        }
-    }
-
-    private void updateSelectedProduct(TableComponent tableComponent) {
+    private void updateSelectedProduct(TableComponent tableComponent, ProductModal productModal) {
         Long productId = null;
 
         // Obtém o modelo da tabela
@@ -205,7 +89,7 @@ public class ProductFrame extends JFrame {
         if (productId != null) {
             // Chame a API para deletar os produtos com os IDs armazenados
             ProductDTO productDTO = getProductById(productId);
-            updateCreateProductDialog(tableComponent, productDTO);
+            productModal.productActionModal(tableComponent, productDTO);
         } else {
             JOptionPane.showMessageDialog(this, "Nenhum produto selecionado para edição.", "Erro", JOptionPane.ERROR_MESSAGE);
         }
@@ -247,16 +131,6 @@ public class ProductFrame extends JFrame {
             JOptionPane.showMessageDialog(this, "Nenhum produto selecionado para deletar.", "Erro", JOptionPane.ERROR_MESSAGE);
         }
 
-    }
-
-    // TODO: FAZER A FUNCIONALIDADE DE FORMA CORRETA
-    private ProductDTO updateProductsFromApi(Long productId) {
-        // Criar uma instância de RestTemplate
-        RestTemplate restTemplate = new RestTemplate();
-
-        // Chama a API para obter o produto
-        String getUrl = API_URL + "/" + productId;
-        return restTemplate.getForObject(getUrl, ProductDTO.class);
     }
 
     // Método para deletar os produtos da API
--- src/main/java/projectpoo/construction_material_store/screens/components/ProductModal.java
+++ src/main/java/projectpoo/construction_material_store/screens/components/ProductModal.java
@@ -18,10 +18,12 @@ public class ProductModal extends JDialog {
     public ProductModal() {
     }
 
-    //TODO:LUCAS CAVALCANTE - Componentizar para o update;
-    public void openCreateProductDialog(TableComponent tableComponent) {
-        JDialog dialog = new JDialog(this, "Criar Produto", true);
+    public void productActionModal(TableComponent tableComponent, ProductDTO productDto) {
+        // Determina se é criação ou edição
+        boolean isEdit = productDto != null;
+        String dialogTitle = isEdit ? "Editar Produto" : "Criar Produto";
 
+        JDialog dialog = new JDialog(this, dialogTitle, true);
         dialog.setSize(650, 400);
         dialog.setLocationRelativeTo(this);
 
@@ -31,35 +33,35 @@ public class ProductModal extends JDialog {
 
         // Campos de entrada
         panel.add(new JLabel("Nome:"));
-        JTextField nameField = new JTextField();
+        JTextField nameField = new JTextField(isEdit ? productDto.getName() : "");
         panel.add(nameField);
 
         panel.add(new JLabel("Código do Produto:"));
-        JTextField codProductField = new JTextField();
+        JTextField codProductField = new JTextField(isEdit ? productDto.getCodProduct() : "");
         panel.add(codProductField);
 
         panel.add(new JLabel("Descrição:"));
-        JTextField descriptionField = new JTextField();
+        JTextField descriptionField = new JTextField(isEdit ? productDto.getDescription() : "");
         panel.add(descriptionField);
 
         panel.add(new JLabel("Quantidade em Estoque:"));
-        JTextField totalStockField = new JTextField();
+        JTextField totalStockField = new JTextField(isEdit ? String.valueOf(productDto.getTotalStock()) : "");
         panel.add(totalStockField);
 
         panel.add(new JLabel("Estoque Mínimo:"));
-        JTextField minStockField = new JTextField();
+        JTextField minStockField = new JTextField(isEdit ? String.valueOf(productDto.getMinStock()) : "");
         panel.add(minStockField);
 
         panel.add(new JLabel("Preço:"));
-        JTextField priceField = new JTextField();
+        JTextField priceField = new JTextField(isEdit ? String.valueOf(productDto.getPrice()) : "");
         panel.add(priceField);
 
         panel.add(new JLabel("Unidade de Venda:"));
-        JTextField salesUnitField = new JTextField();
+        JTextField salesUnitField = new JTextField(isEdit ? productDto.getSalesUnit() : "");
         panel.add(salesUnitField);
 
         panel.add(new JLabel("Data de Validade: (dd-mm-aaaa)"));
-        JTextField expirationDateField = new JTextField();
+        JTextField expirationDateField = new JTextField(isEdit ? productDto.getExpirationDate() : "");
         panel.add(expirationDateField);
 
         panel.add(new JLabel("Categorias:"));
@@ -68,10 +70,12 @@ public class ProductModal extends JDialog {
                 "Materiais Hidráulicos", "Madeiras e Compensados", "Ferragens", "Pisos e Revestimentos",
                 "Iluminação", "Decoração", "Jardinagem", "Outros"
         });
+        // Define a categoria selecionada com base no valor do DTO
+        categoryList.setSelectedItem(isEdit ? productDto.getCategory() : "");
         panel.add(categoryList);
 
         // Botão de salvar
-        JButton btnSave = new JButton("Salvar");
+        JButton btnSave = new JButton(isEdit ? "Atualizar" : "Salvar");
         btnSave.addActionListener(e -> {
             String name = nameField.getText();
             String codProduct = codProductField.getText();
@@ -83,6 +87,10 @@ public class ProductModal extends JDialog {
             String expirationDate = expirationDateField.getText();
             String category = categoryList.getSelectedItem().toString();
 
+            double totalStock = Double.parseDouble(totalStockStr);
+            double minStock = Double.parseDouble(minStockStr);
+            double price = Double.parseDouble(priceStr);
+
             // Validação dos campos
             if (name.isEmpty() || codProduct.isEmpty() || totalStockStr.isEmpty() ||
                     minStockStr.isEmpty() || priceStr.isEmpty() || salesUnit.isEmpty()) {
@@ -90,20 +98,40 @@ public class ProductModal extends JDialog {
                 return;
             }
 
-            try {
-                double totalStock = Double.parseDouble(totalStockStr);
-                double minStock = Double.parseDouble(minStockStr);
-                double price = Double.parseDouble(priceStr);
+            if (totalStock < 0 || minStock < 0 || price < 0) {
+                JOptionPane.showMessageDialog(dialog, "Valores numéricos não podem ser negativos.", "Erro", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
 
-                if (totalStock < 0 || minStock < 0 || price < 0) {
-                    JOptionPane.showMessageDialog(dialog, "Valores numéricos não podem ser negativos.", "Erro", JOptionPane.ERROR_MESSAGE);
-                    return;
+            // Criar produto
+            btnSave.setEnabled(false); // Desabilita o botão para evitar múltiplos cliques
+
+            try {
+
+                // Criação ou atualização
+                if (isEdit) {
+                    // Atualizar o produto existente
+                    productDto.setName(name);
+                    productDto.setCodProduct(codProduct);
+                    productDto.setDescription(description);
+                    productDto.setTotalStock(totalStock);
+                    productDto.setMinStock(minStock);
+                    productDto.setPrice(price);
+                    productDto.setSalesUnit(salesUnit);
+                    productDto.setExpirationDate(expirationDate);
+                    productDto.setCategory(category);
+
+                    updateProduct(productDto); // Chame o método de atualização
+                    JOptionPane.showMessageDialog(dialog, "Produto atualizado com sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    // Cria o objeto Product com os novos atributos
+                    ProductDTO newProduct = new ProductDTO(name, codProduct, totalStock, description, minStock, price, salesUnit, expirationDate, category);
+
+                    // Criar um novo produto
+                    createProduct(newProduct);
+                    JOptionPane.showMessageDialog(dialog, "Produto criado com sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
                 }
 
-                // Criar produto
-                btnSave.setEnabled(false); // Desabilita o botão para evitar múltiplos cliques
-                createProduct(name, codProduct, totalStock, description, minStock, price, salesUnit, expirationDate, category);
-                JOptionPane.showMessageDialog(dialog, "Produto criado com sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
                 tableComponent.loadData(API_URL, ProductDTO[].class); // Atualizar lista de produtos
                 dialog.dispose(); // Fechar o diálogo
             } catch (DateTimeParseException ex) {
@@ -125,21 +153,8 @@ public class ProductModal extends JDialog {
         dialog.setVisible(true);
     }
 
-    private void createProduct(
-            String name,
-            String codProduct,
-            double totalStock,
-            String description,
-            double minStock,
-            double price,
-            String salesUnit,
-            String expirationDate,
-            String category
-    ) throws Exception {
+    private void createProduct(ProductDTO newProduct) throws Exception {
         RestTemplate restTemplate = new RestTemplate();
-
-        // Cria o objeto Product com os novos atributos
-        ProductDTO newProduct = new ProductDTO(name, codProduct, totalStock, description, minStock, price, salesUnit, expirationDate, category);
 
         // Envia a requisição POST
         HttpEntity<ProductDTO> request = new HttpEntity<>(newProduct);
@@ -147,6 +162,19 @@ public class ProductModal extends JDialog {
 
         if (!response.getStatusCode().is2xxSuccessful()) {
             throw new Exception("Erro ao criar produto: " + response.getStatusCode());
+        }
+    }
+
+    private void updateProduct(ProductDTO productDto) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Envia a requisição PUT com o identificador do produto
+        String url = API_URL + "/" + productDto.getId();  // Supondo que o ID do produto seja parte da URL para atualização
+        HttpEntity<ProductDTO> request = new HttpEntity<>(productDto);
+        ResponseEntity<Product> response = restTemplate.exchange(url, HttpMethod.PUT, request, Product.class);
+
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            throw new Exception("Erro ao atualizar produto: " + response.getStatusCode());
         }
     }
 }
